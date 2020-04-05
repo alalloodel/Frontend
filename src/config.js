d5a7b88b2f4f7d7538415d66fdcb5cade8f50c26
alalloodel <alalloo@deloitte.co.za>
2020-04-05 22:01:20 +0200

    Setting up React

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-apps-uploads-03042020"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://9zs8stwrt6.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_v4vepdLBd",
+    APP_CLIENT_ID: "6jeon7ck9jagjsj8esgvq0uhau",
+    IDENTITY_POOL_ID: "us-east-2:7ec19706-6319-4161-8977-3b5929ba4687"
+  }
+};
